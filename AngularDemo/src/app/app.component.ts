c05d3320f52810d51a528a01f9ca7ae5d0b4125f
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 16:21:19 +0700

    Learning Angular in 10 days from CSharp Corner

---
+++ AngularDemo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'app-root',  
+  template: '<h1>Component is the main Building Block in Angular</h1> <h2>Angular 8 Samples</h2>',  
+  styles: ['h1{color:red;font-weight:bold}','h2{color:blue}']  
+})  
+export class AppComponent {  
+    
+}  
