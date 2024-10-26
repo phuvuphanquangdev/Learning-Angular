e7c66d5fa87a11ac103acff149aa6093c7182762
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-26 16:29:01 +0700

    Angular HTTP Request

--- Consuming-HTTP-Services/README.md
+++ Consuming-HTTP-Services/README.md
@@ -1,6 +1,6 @@
-# ConsumingHTTPServices
+# NgCompleteGuideUpdate
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.5.
 
 ## Development server
 
--- Consuming-HTTP-Services/src/app/app.component.spec.ts
+++ Consuming-HTTP-Services/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Consuming-HTTP-Services'`, () => {
+  it(`should have as title 'ng-complete-guide-update'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Consuming-HTTP-Services');
+    expect(app.title).toEqual('ng-complete-guide-update');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Consuming-HTTP-Services app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-complete-guide-update!');
   });
 });
